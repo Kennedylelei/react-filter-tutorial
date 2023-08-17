883a059ed8b3b909ab79766e1d37bbf16f09b328
Ken <kennedylelei26@gmail.com>
2023-08-17 14:18:03 +0300

    Added function to fetch data using movies api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    fetchPopular();
+  },[]);
+
+  const fetchPopular = async () => {
+    const data  = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=43d18fba9655b83f394c1f6635de671e&language=en=US&page=1"
+    );
+    const movies = await data.json();
+    console.log(movies)
+    setPopular(movies.results);
+  };
+  
   return (
     <div className="App">
       <h1>Hello</h1>
