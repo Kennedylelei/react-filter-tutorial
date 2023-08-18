52bb2164819c256cad76c6e844fd60d7964acc56
Ken <kennedylelei26@gmail.com>
2023-08-18 13:40:52 +0300

    Added transitions

--- src/movie.js
+++ src/movie.js
@@ -2,7 +2,13 @@ import { motion } from "framer-motion";
 
 function Movie({movie}) {
     return(
-        <motion.div layout>
+        <motion.div 
+            layout
+            animate = {{ opacity: 1 }}
+            intial = {{ opacity: 0 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3}}
+            >
             <h2>{movie.title}</h2>
             <img 
                 src={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path}
